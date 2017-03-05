1e63c3237ea87c9f4d34f1f88bf050efec08e9a5
brainsandspace <drbaker2@gmail.com>
2017-03-04 23:34:24 -0500

    adding middleware to handle webhooks

--- server/index.js
+++ server/index.js
@@ -12,6 +12,11 @@ const app = express();
 
 // If you need a backend, e.g. an API, add your custom backend-specific middleware here
 // app.use('/api', myApi);
+app.use('/webhook', (req, res, next) => {
+  console.log(req.originalUrl);
+  console.log(req.accepted.method);
+  console.log(JSON.stringify(req.body));
+});
 
 // In production we need to pass these values in instead of relying on webpack
 setup(app, {
